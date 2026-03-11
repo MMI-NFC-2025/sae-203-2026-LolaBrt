0d781240832d623b5e189a75af9e58c73bb1ad6c
LolaBrt <lola.brouart@edu.univ-fcomte.fr>
2026-03-11 10:30:30 +0100

    card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
+
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sae203-2026.lola-brouart.fr"
+      }
+    ]
+  },
 
   vite: {
     plugins: [tailwindcss()]
