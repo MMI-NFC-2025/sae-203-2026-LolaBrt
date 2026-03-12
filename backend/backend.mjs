e1b326b3f61cefb331618a16daf419091f24a7f4
LolaBrt <lola.brouart@edu.univ-fcomte.fr>
2026-03-12 07:52:02 +0100

    filtre

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -3,14 +3,16 @@ const pb = new PocketBase('https://sae203-2026.lola-brouart.fr');
 
 export async function allArtistsDate() { 
     const records = await pb.collection('ARTISTE').getFullList({
-        sort: 'show_artiste'
+        sort: 'show_artiste',
+        expand: 'performe'
     }); 
     return records; 
 }
 
 export async function allArtists() { 
     const records = await pb.collection('ARTISTE').getFullList({
-        sort: 'nom_artiste'
+        sort: 'nom_artiste',
+        expand: 'performe'
     }); 
     return records; 
 }
@@ -36,6 +38,7 @@ export async function artistesParScene(sceneId) {
   const artistes = await pb.collection("ARTISTE").getFullList({
     filter: `performe = "${sceneId}"`,
     sort: "show_artiste",
+    expand: "performe",
   });
   return artistes;
 }
@@ -43,6 +46,7 @@ export async function artistesParScene(sceneId) {
 export async function artistesParNomScene(nomScene) {
   const scenes = await pb.collection("SCENE").getFullList({
     filter: `nom_scene ~ "${nomScene}"`,
+    expand: "performe",
   });
 
   if (scenes.length === 0) {
@@ -73,4 +77,28 @@ export async function Userauth(email, password) {
 
 export async function getImageUrl(record, recordImage) {
     return pb.files.getURL(record, recordImage);
+}
+
+export async function artistesParGenre(genre) {
+  return await pb.collection("ARTISTE").getFullList({
+    filter: `genre_music_artiste = "${genre}"`,
+    sort: "show_artiste",
+    expand: "performe",
+  });
+}
+
+export async function artistesParDate(dateISO) {
+  // dateISO attendu: "2026-08-28"
+  const debut = `${dateISO} 00:00:00.000Z`;
+
+  const d = new Date(`${dateISO}T00:00:00.000Z`);
+  d.setUTCDate(d.getUTCDate() + 1);
+  const nextDate = d.toISOString().substring(0, 10);
+  const fin = `${nextDate} 00:00:00.000Z`;
+
+  return await pb.collection("ARTISTE").getFullList({
+    filter: `show_artiste >= "${debut}" && show_artiste < "${fin}"`,
+    sort: "show_artiste",
+    expand: "performe",
+  });
 }
